342710de5d7aa9272e30095fa1faee962a36c167
nikofox <2020104216@qq.com>
2026-03-08 09:27:41 +0800

    20260308A

---
+++ 前后端交互/03Webpack/code/08/src/login/index.js
@@ -0,0 +1,43 @@
+// import  {checkPhone,checkCode} from  '../utils/check.js'
+const {checkPhone,checkCode} = require('../utils/check.js')
+document.querySelector('.btn').addEventListener('click',()=>{
+    const phone = document.querySelector('.login-form [name="mobile"]').value
+    const code = document.querySelector('.login-form [name="code"]').value
+
+    //然后执行校验逻辑
+
+    if(!checkPhone(phone)){
+        console.log('号码格式有误')
+        return //有误则阻止代码继续向下运行
+    }
+
+    if(!checkCode(code)){
+        console.log('验证码长度必须为6位')
+        return
+    }
+
+    console.log('提交到服务器登录')
+})  
+
+
+//引入bootstrap
+require('bootstrap/dist/css/bootstrap.min.css')
+require('bootstrap/dist/js/bootstrap.min.js')
+
+
+//引入css文件
+require('./index.css')
+
+//引入less文件  
+require('./index.less')
+
+
+//js引入本地图片资源要用import方式(ECMAScript标准),如果是http地址,字符串可以直接写
+const imgObj  = require('./assets/logo.png')
+
+const theImg = document.createElement('img')
+theImg.src =  imgObj
+
+document.querySelector('.login-wrap').appendChild(theImg)
+
+
